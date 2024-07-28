0635fad57bd4aedc2d173d20d19f638dd016a9ae
maruki00 <aitbaha48@gmail.com>
2024-07-27 20:44:15 -0400

    --

--- internal/domain/customer/OfflineRepo/memoryRepo.go
+++ internal/domain/customer/OfflineRepo/memoryRepo.go
@@ -21,14 +21,25 @@ func (o *CustomerMemoryRepo)Get(id int) (*aggrigate.Customer, error) {
 	if ok != nil {
 		return nil,errors.New("Could not get the customer")
 	}
-
 	return item, nil
 
 }
 
 func (o *CustomerMemoryRepo)Add(customer aggrigate.Customer) error {
 
+	if o.items == nil{
+		o.Lock()
+		o.items = make(map[int]aggrigate.Custmer, 0)
+		o.Unlock()
+	}
 
+	if _, ok = c.items[custmer.ID]; ok {
+		return errors.New("customer Already Exists")
+	}
+	o.Lock()
+	o.items[c.ID] = c
+	o.Unlock()
+	return nil
 }
 
 func (o *CustomerMemoryRepo)Update(customer aggrigate.Customer) error {
---
+++ internal/services/order.go
@@ -0,0 +1,33 @@
+package services
+
+type OrderConfiguration func(os *orderService) error
+
+type OrderService struct {
+	customers customer.CustomerRespository
+}
+
+func NewOrderService(cfgs ...OrderConfiguration) (*OrderService, error) {
+
+	os := OrderService{}
+
+	for _, cfg := range cfgs {
+		err := cfg(os)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+	return os, nil
+}
+
+func WithCustomerRepository(cr custmer.CustomerRespository) OrderConfiguration {
+	return func(os *orderService) error {
+		os.customers = cr
+		return nil
+	}
+}
+
+func WithMemoryCustomerRepository(cr custmer.CustomerRespository) OrderConfiguration {
+	cr := offlinerepo.New()
+	return WithCustomerRepository(cr)
+}
